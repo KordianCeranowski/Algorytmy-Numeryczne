614e2115238e276ecac63b58e49290cc542f31ff
KordianCeranowski <KordianCeranowski@users.noreply.github.com>
2019-10-10 21:36:41 +0200

    basic logn zaimplementowany

---
+++ proj1/Logn/src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static void main(String[] args) {
+
+        long startTime = System.nanoTime();
+
+        Ln ln = new Ln(1.4, 1000000);
+
+        System.out.println("Math.logn( " + ln.x + " ) = " + ln.trueLn);
+        System.out.println("Naive            = " + ln.getSum(ln.naiveValues));
+        System.out.println("Smart            = " + ln.getSum(ln.smartValues));
+        System.out.println();
+
+        long endTime = System.nanoTime();
+        System.out.println(((endTime - startTime)/1000000));
+
+//
+//        long TEST = 50;
+//        long startTime = System.nanoTime();
+//        System.out.println(MyMath.pow(2,15));
+//        long endTime = System.nanoTime();
+//        System.out.println((endTime - startTime/1000000));
+//
+//        startTime = System.nanoTime();
+//        System.out.println(MyMath.power(2,15));
+//        endTime = System.nanoTime();
+//        System.out.println((endTime - startTime));
+
+
+    }
+}
