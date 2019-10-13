bf85ac34347f286b379d7d004f93dd81a8b56c02
KordianCeranowski <KordianCeranowski@users.noreply.github.com>
2019-10-13 19:41:06 +0200

    up

--- proj1/Logn/src/Main.java
+++ proj1/Logn/src/Main.java
@@ -3,15 +3,18 @@ public class Main {
 
         long startTime = System.nanoTime();
 
-        Ln ln = new Ln(1.22, 1000000);
+        Ln ln = new Ln(0.0001, 1000000);
         System.out.println();
-        long endTime = System.nanoTime();
-        System.out.println(((endTime - startTime)/1000000 + "ms"));
+        System.out.println(((System.nanoTime() - startTime)/1000000 + "ms"));
+
+
+        //new PrecisionTest(0, 0.01, 0.0001, 1000000);
+
 
         startTime = System.nanoTime();
-        ln.packDataToCSV();
-        endTime = System.nanoTime();
-        System.out.println(((endTime - startTime)/1000000 + "ms"));
+        //ln.packDataToCSV();
+        ln.packShortDataToCSV();
+        System.out.println(((System.nanoTime() - startTime)/1000000 + "ms"));
 
     }
 }
---
+++ proj1/Logn/src/PrecisionTest.java
@@ -0,0 +1,50 @@
+import java.io.FileWriter;
+import java.util.ArrayList;
+
+public class PrecisionTest {
+    private static final String SEPARATOR = ";";
+    ArrayList<Double> currX;
+    ArrayList<Integer> NN;
+    ArrayList<Integer> SN;
+    ArrayList<Integer> NR;
+    ArrayList<Integer> SR;
+
+    public PrecisionTest(double startX, double endX, double differential, int maxN) {
+        double startTime = System.nanoTime();
+        currX = new ArrayList<>();
+        NN = new ArrayList<>();
+        SN = new ArrayList<>();
+        NR = new ArrayList<>();
+        SR = new ArrayList<>();
+        Ln tempLn;
+        for(double x = startX; x <= endX; x += differential){
+            tempLn = new Ln(x, maxN);
+            currX.add(x);
+            NN.add(tempLn.nForNaiveNormalPrecision);
+            SN.add(tempLn.nForSmartNormalPrecision);
+            NR.add(tempLn.nForNaiveReversedPrecision);
+            SR.add(tempLn.nForSmartReversedPrecision);
+        }
+
+        packDataToCSV(startX, endX, (int)((endX-startX)/differential));
+
+        System.out.println(("Proces zajął " + (System.nanoTime() - startTime)/1000000 + "ms"));
+
+    }
+
+    public void packDataToCSV(double start, double stop, int times){
+        try{
+            System.out.println("Saving precision data...");
+            FileWriter fw=new FileWriter("C:\\Users\\Kordian\\Desktop\\LnData\\Wyniki badania precyzji(" +start + ", " + stop + ").csv");
+            fw.write( "X" +SEPARATOR+ "NaiveNormal" +SEPARATOR+ "NaiveReversed" +SEPARATOR+ "SmartNormal" +SEPARATOR+ "SmartReversed\n");
+
+            for (int i = 0; i<times; i++) {
+                fw.write((currX.get(i) +SEPARATOR+ NN.get(i) +SEPARATOR+ SN.get(i) +SEPARATOR+ NR.get(i) +SEPARATOR+ SR.get(i) + "\n").replace('.', ','));
+            }
+
+            fw.close();
+        }catch(Exception e){System.out.println(e);}
+        System.out.println("Precision Data extracted...");
+    }
+
+}
