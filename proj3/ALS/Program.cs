a6fc18bc6f6d677984d7ef516dbb4bdd7a437d76
kordianceranowski <kordianceranowski@gmail.com>
2019-12-01 16:47:10 +0100

    proj3 start

---
+++ proj3/ALS/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ALS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Parser parser = new Parser("10.txt");
+
+            var R = parser.GetR();
+
+
+            Console.Beep();
+        }
+    }
+}
+
