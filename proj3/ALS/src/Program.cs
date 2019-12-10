ecb21d57de436b8e5fc9a394a45a643bc71362aa
KordianCeranowski <kordianceranowski+github@gmail.com>
2019-12-10 15:38:02 +0100

    +pliki

--- proj3/ALS/src/Program.cs
+++ proj3/ALS/src/Program.cs
@@ -7,25 +7,13 @@ namespace RecommenderSystem
     {
         static void Main(string[] args)
         {
-            Stopwatch stopwatch = new Stopwatch();
 
-            stopwatch.Start();
-            ALS als = new ALS(10, 100, 100);
-            Console.WriteLine("Czas na czytanie z pliku .txt\t" + stopwatch.ElapsedMilliseconds / 1000 + "s");
+            RMatrix R1 = Serializer.UnpackRMatrix("p1050u10050");
 
-            stopwatch.Restart();
-            ALS als2 = new ALS(10, "p100u100");
-            Console.WriteLine("Czas na czytanie z pliku .rmat\t" + stopwatch.ElapsedMilliseconds / 1000 + "s");
 
-            Console.WriteLine("\n.txt");
-            als.HidingTest(0.1, 10, 0.1);
-            Console.WriteLine(stopwatch.ElapsedMilliseconds / 1000 + "s");
+            Console.WriteLine("stop");
+            //ALS als = new ALS(10, 550, 5050);
 
-            Console.WriteLine("\n.rmat");
-            als2.HidingTest(0.1, 10, 0.1);
-            Console.WriteLine(stopwatch.ElapsedMilliseconds / 1000 + "s");
-
-            Console.WriteLine();
         }
     }
 }
