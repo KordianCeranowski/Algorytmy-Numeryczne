42c291914fc44b66073c79f15ca6dcc6f2143c80
KordianCeranowski <kordianceranowski+github@gmail.com>
2019-12-10 17:37:17 +0100

    funkcja nowa Marcinowa

--- proj3/ALS/src/ObjectiveFunction.cs
+++ proj3/ALS/src/ObjectiveFunction.cs
@@ -1,42 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Microsoft.VisualBasic.CompilerServices;
 
 namespace RecommenderSystem
 {
-	static class ObjectiveFunction
-	{
-		//static double lastResult = 1;
-		public static double Calculate(RMatrix R, Matrix U, Matrix P, double lambda)
-		{
-			double firstSum = 0, secondSum = 0, thirdSum = 0;
+    static class ObjectiveFunction
+    {
+        public static double Calculate(RMatrix R, Matrix U, Matrix P, double lambda)
+        {
+            double firstSum = 0, secondSum = 0, result = 0;
+            Matrix uMatrix, pMatrix;
 
-			for (int u = 0; u < U.ColumnCount; u++)
-			{
-				for (int p = 0; p < P.ColumnCount; p++)
-				{
-					firstSum += Math.Pow(R[u, p] - (U.GetVector(u).GetTransposed() * P.GetVector(p)).Data[0, 0], 2);
-				}
-			}
+            for (int u = 0; u < U.ColumnCount; u++)
+            {
+                uMatrix = U.GetVector(u);
 
-			for (int u = 0; u < U.ColumnCount; u++)
-			{
-				secondSum += U.GetVector(u).GetSquaredNorm();
-			}
+                for (int p = 0; p < P.ColumnCount; p++)
+                {
+                    pMatrix = P.GetVector(p);
 
-			for (int p = 0; p < P.ColumnCount; p++)
-			{
-				thirdSum += P.GetVector(p).GetSquaredNorm();
-			}
+                    result += Math.Pow(R[u, p] - (uMatrix.GetTransposed() * pMatrix).Data[0, 0], 2);
+                    firstSum += pMatrix.GetSquaredNorm();
+                }
+                secondSum += uMatrix.GetSquaredNorm();
+            }
 
-			double result = firstSum + lambda * (secondSum + thirdSum);
+            result += lambda * (firstSum + secondSum);
 
-            //Console.WriteLine($"{result/lastResult*100}");
             Console.WriteLine(result);
-            //lastResult = result;
 
-		    return result;
-		}
-	}
+            return result;
+        }
+    }
 }
