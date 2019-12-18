5c06d1990b7f0f1de3605b0e0f4aa6a206671ea0
Marcin Borysiak <marcinez24@gmail.com>
2019-12-18 14:20:09 +0100

    Update ObjectiveFunction.cs
    Added if (R[u,p] != 0)

--- proj3/ALS/src/ObjectiveFunction.cs
+++ proj3/ALS/src/ObjectiveFunction.cs
@@ -2,47 +2,50 @@
 
 namespace RecommenderSystem
 {
-    internal static class ObjectiveFunction
-    {
-        public static double Calculate(int d, RMatrix R, Matrix U, Matrix P, double lambda)
-        {
-            double firstSum = 0, secondSum = 0, result = 0;
+	internal static class ObjectiveFunction
+	{
+		public static double Calculate(int d, RMatrix R, Matrix U, Matrix P, double lambda)
+		{
+			double firstSum = 0, secondSum = 0, result = 0;
 
-            for (int u = 0; u < U.ColumnCount; u++)
-            {
-                for (int p = 0; p < P.ColumnCount; p++)
-                {
+			for (int u = 0; u < U.ColumnCount; u++)
+			{
+				for (int p = 0; p < P.ColumnCount; p++)
+				{
+					if (R[u, p] != 0)
+					{
+						double sum = 0;
 
-                    double sum = 0;
-                    for (int row = 0; row < d; row++)
-                    {
-                        sum += U.Data[row, u] * P.Data[row, p];
-                    }
-                    result += Math.Pow((R[u, p] - sum), 2);
+						for (int row = 0; row < U.RowCount; row++)
+						{
+							sum += U.Data[row, u] * P.Data[row, p];
+						}
+						result += Math.Pow((R[u, p] - sum), 2);
+					}
+					
+					double squaredNormFromP = 0;
 
-                    double squaredNormFromP = 0;
-                    for (int row = 0; row < d; row++)
-                    {
-                        squaredNormFromP += Math.Pow(P.Data[row, p], 2);
-                    }
-                    firstSum += squaredNormFromP;
+					for (int row = 0; row < P.RowCount; row++)
+					{
+						squaredNormFromP += Math.Pow(P.Data[row, p], 2);
+					}
+					firstSum += squaredNormFromP;
+				}
 
-                }
+				double squaredNormFromU = 0;
 
-                double squaredNormFromU = 0;
-                for (int row = 0; row < d; row++)
-                {
-                    squaredNormFromU += Math.Pow(U.Data[row, u], 2);
-                }
-                secondSum += squaredNormFromU;
+				for (int row = 0; row < d; row++)
+				{
+					squaredNormFromU += Math.Pow(U.Data[row, u], 2);
+				}
+				secondSum += squaredNormFromU;
+			}
 
-            }
+			result += lambda * (firstSum + secondSum);
 
-            result += lambda * (firstSum + secondSum);
+			Console.WriteLine(result);
 
-            Console.WriteLine(result);
-
-            return result;
-        }
-    }
+			return result;
+		}
+	}
 }
